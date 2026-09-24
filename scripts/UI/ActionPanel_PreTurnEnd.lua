5dfe9191a57ab798bff9ec99fa3a35e022a24d97
satoon101 <satoon101@gmail.com>
2026-09-23 22:26:42 -0400

    Added missing argument to DoEndTurn function.

--- scripts/UI/ActionPanel_PreTurnEnd.lua
+++ scripts/UI/ActionPanel_PreTurnEnd.lua
@@ -4,10 +4,10 @@ include("ActionPanel_Expansion2");
 
 BASE_DoEndTurn = DoEndTurn
 
-function DoEndTurn()
+function DoEndTurn(optionalNewBlocker)
     local playerID = Game.GetLocalPlayer()
     LuaEvents.PreTurnEnd(playerID)
-    BASE_DoEndTurn()
+    BASE_DoEndTurn(optionalNewBlocker)
 end
 
 print("=== Auto Production Queue (ActionPanel) Loaded ===")
